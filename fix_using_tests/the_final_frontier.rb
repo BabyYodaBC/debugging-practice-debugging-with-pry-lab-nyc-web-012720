d98480c68792e51aff1f9ad8a6272770bdb71ef9
ricky wahba <rjwahba@gmail.com>
2020-01-22 19:26:36 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 def generate_star_date
   (rand(100000) + 400000) / 10.0
 end
@@ -18,6 +20,6 @@ end
 def engage
   date = generate_star_date
   state_log(date)
-  crew
+  binding.pry
   greet_crew(crew)
 end
