c9e6bb00f34e439d04363157ff36325c3b29d513
ricky wahba <rjwahba@gmail.com>
2020-01-22 19:37:29 +0000

    Done.

--- first_priority/that_was_unexpected.rb
+++ first_priority/that_was_unexpected.rb
@@ -1,9 +1,11 @@
+require "pry"
+
 class Person
   attr_accessor :name, :candy_lover
 
   def initialize(name, candy_lover)
-    @name = name
-    @candy_lover = candy_lover
+    name = name
+    candy_lover = candy_lover
   end
 
   def tall?
@@ -19,11 +21,12 @@ class Person
   end
 
   def ballin_candy_lover?
-    if @candy_lover == true
+    if candy_lover = true
       "Aw sweet, this cat loves them some candy!"
     else
       10.times do
         "not a ballin candy lover"
     end
   end
+end
 end
